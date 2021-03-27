0fe4be94122d00fdcd2db6e9ae7957ee2a610e58
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-03-27 13:50:38 +0530

    Publish/Subscribe

--- receive_logs.py
+++ receive_logs.py
@@ -1,3 +1,4 @@
+import time
 import pika
 import sys
 import os
@@ -7,15 +8,18 @@ def main():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-    channel.queue_declare(queue='hello')
+    result = channel.queue_declare(queue='', exclusive=True)
+    queue_name = result.method.queue
+    channel.queue_bind(exchange='logs', queue=queue_name)
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
-
+        print(" [x] Received %r" % body.decode())
+        time.sleep(5)
+        print(" [x] Done")
     channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
-
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
